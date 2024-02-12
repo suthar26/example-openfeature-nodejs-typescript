8f8f26cc40fa2eeea0eb63d32b3ca80f2b69ef0d
Jonathan Norris <jonathan@taplytics.com>
2024-02-12 17:03:34 -0500

    fix unit tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,30 +1,44 @@
 import run from "./app";
 import request from "supertest";
 import { DVCVariableValue } from "@devcycle/nodejs-server-sdk";
+import { DVCVariableInterface } from "@devcycle/js-cloud-server-sdk/src/types";
 
 let mockDevCycleClient = {
   onClientInitialized: jest.fn(),
   variableValue: jest.fn(),
+  variable: jest.fn(),
 };
+
+let mockOpenFeatureClient = {
+    getBooleanValue: jest.fn(),
+    getStringValue: jest.fn(),
+};
+
 jest.mock("./devcycle", () => ({
-  initializeDevCycleClient: () => mockDevCycleClient,
+  initializeDevCycleWithOpenFeature: () => ({
+      devcycleClient: mockDevCycleClient,
+      openFeatureClient: mockOpenFeatureClient
+  }),
   getDevCycleClient: () => mockDevCycleClient,
+  getOpenFeatureClient: () => mockOpenFeatureClient,
 }));
 jest.mock("./utils/logVariation");
 
 describe("greeting", () => {
-  const mockVariableValue = (variable: string, value: DVCVariableValue) => {
-    mockDevCycleClient.variableValue = jest
-      .fn()
-      .mockImplementation((user, variableName, defaultValue) =>
-        variableName === variable ? value : defaultValue
-      );
+
+  const mockVariable = (key: string, value: DVCVariableValue, type: DVCVariableInterface['type']) => {
+    mockOpenFeatureClient.getBooleanValue.mockImplementation((variableKey: string, defaultValue: Boolean) => {
+        return Promise.resolve((type === 'Boolean' && variableKey === key) ? value : defaultValue)
+    });
+    mockOpenFeatureClient.getStringValue.mockImplementation((variableKey: string, defaultValue: string) => {
+        return Promise.resolve((type === 'String' && variableKey === key) ? value : defaultValue)
+    });
   };
 
   test.each(["default", "step-1", "step-2", "step-3"])(
     'returns greeting for variable value "%s"',
     async (value) => {
-      mockVariableValue("example-text", value);
+      mockVariable("example-text", value, 'String');
 
       const app = await run();
 
